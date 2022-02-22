8ea3c0ff125031fa4d3efb8bd9f0bfa61647fc55
Spupu <alexanderquach@gmail.com>
2022-02-22 11:21:15 -0800

    Create UnityExample.txt
    New File as demonstration

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public Transform goal;
+    
+    void Start(){
+       
+
+    }
+
+    void Update(){
+        NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10f;
+    public float vertInput;
+    public float horiInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vertInput = Input.GetAxis("Vertical");
+        horiInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * vertInput);
+        transform.Translate(Vector3.right * speed * Time.deltaTime * horiInput);
+    }
+}
